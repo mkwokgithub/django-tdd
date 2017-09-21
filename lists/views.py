84169cb0c040545b9673bf3528f6260e1b5b7b35
Yuk Ming Kwok <mkwok@cisco.com>
2017-09-21 14:12:34 -0700

    chapter 5 redirect after a post

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from lists.models import Item
 
 
@@ -9,17 +8,8 @@ from lists.models import Item
 def home_page(request):
 
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
     
-    #if request.method == 'POST':
-    #    return HttpResponse(request.POST['item_text'])
-
-
-    # pass variables from python view code to html templates
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text
-        })
+    return render(request, 'home.html')
 
