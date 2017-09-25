09c4c91d9cf99039f40f5044f8749a3ff0e368e2
Yuk Ming Kwok <mkwok@cisco.com>
2017-09-25 14:32:41 -0700

    adjust model with foreign key between list and item db tables

--- lists/models.py
+++ lists/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Item(models.Model):
+
+class List(models.Model):
     text = models.TextField(default='')
     pass
+
+
+class Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List,default=None)
+    pass
+
